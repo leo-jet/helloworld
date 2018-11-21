6a1152b1e40a8183070d9ecbfd6a6b47943d4dc2
TEMATIO J. EMMANUEL <emmanuel.tematio@gmail.com>
2018-11-21 23:12:54 +0100

    static url with /

--- homeschool_production/settings.py
+++ homeschool_production/settings.py
@@ -219,7 +219,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'homeschoolStatic'
+STATIC_URL = 'homeschoolStatic/'
 STATIC_ROOT = 'homeschoolStatic'
 
 STATICFILES_DIRS = (
